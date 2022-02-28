b19584853ef4be16b4682a8f6de563a8b1b12ddc
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-02-28 14:20:12 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flower_info/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flower_info
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flower_info/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flower Info',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+
+
+
+
