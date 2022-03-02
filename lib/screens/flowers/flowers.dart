8e333769a438b8833f8d6d37263add9f367f974d
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-03-03 00:04:05 +0530

    Added flowers.dart

---
+++ lib/screens/flowers/flowers.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class FLowers extends StatelessWidget {
+  const FLowers({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Flowers list"),
+    );
+  }
+}
