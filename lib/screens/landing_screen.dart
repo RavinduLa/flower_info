09744d6ce0a0436d1c98ae1f19a3d4e03e8e10ca
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-03-03 00:01:37 +0530

    Added landing_screen.dart

---
+++ lib/screens/landing_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatefulWidget {
+  const LandingScreen({Key? key}) : super(key: key);
+
+  @override
+  _LandingScreenState createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
