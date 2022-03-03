810c7bca17b5010e48233b41e7268758e6e4bdf6
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-03-04 01:34:18 +0530

    Added application_state.dart

---
+++ lib/providers/application_state.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flower_info/models/flower_model.dart';
+import 'package:flutter/material.dart';
+
+class ApplicationState extends ChangeNotifier {
+  ApplicationState() {
+    init();
+  }
+
+  StreamSubscription<QuerySnapshot>? _flowerListSubscription;
+  List<Flower> _flowers = [];
+  List<Flower> get flowers => _flowers;
+
+  void init() async {
+    _flowerListSubscription = FirebaseFirestore.instance
+        .collection('flowers')
+        .orderBy('timestamp', descending: true)
+        .snapshots()
+        .listen((snapshot) {
+      _flowers = [];
+      for (final document in snapshot.docs) {
+        _flowers.add(
+          Flower(
+            commonName: document.data()['commonName'] as String,
+            scientificName: document.data()['scientificName'] as String,
+            matureSize: document.data()['matureSize'] as String,
+            nativeRegion: document.data()['nativeRegion'] as String,
+            imageLink: document.data()['imageLink'] as String,
+          ),
+        );
+      }
+      notifyListeners();
+    });
+  }
+}
