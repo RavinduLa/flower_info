5e96a9886c5b80ae8a49d17585acd19a5ada4de2
Navinda Lankesh <adilanga@gmail.com>
2022-03-09 01:58:08 +0530

    Disease - Admin Dashboard

--- lib/screens/admin/admin_dashboard.dart
+++ lib/screens/admin/admin_dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:flower_info/screens/diseases/disease_admin.dart';
 import 'package:flutter/material.dart';
 
 import '../flowers/flower_admin_list.dart';
@@ -30,7 +31,9 @@ class AdminDashboard extends StatelessWidget {
             child: Text('Fertilizers Panel'),
           ),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.pushNamed(context, DiseaseAdmin.routeName);
+            },
             child: Text('Diesases Panel Panel'),
           ),
         ],
---
+++ lib/screens/diseases/disease_admin.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class DiseaseAdmin extends StatelessWidget {
+  const DiseaseAdmin({Key? key}) : super(key: key);
+
+  static String routeName = "/admin/disease/disease-list";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Diseases Admin Panel'),
+      ),
+      body: const Center(
+        child: Text('Diseases Admin list'),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () {
+      //     Navigator.pushNamed(context, AddFlower.routeName);
+      //   },
+      //   child: const Icon(Icons.add),
+      // ),
+    );
+  }
+}
