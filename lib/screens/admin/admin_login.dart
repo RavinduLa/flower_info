7c1371045dc98ff20e15322e4879e85053c5152f
Hirush Gimhan <it19240848@my.sliit.lk>
2022-03-09 23:34:09 +0530

    Created admin login class

---
+++ lib/screens/admin/admin_login.dart
@@ -0,0 +1,16 @@
+import 'package:flower_info/components/login/body.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AdminLogin extends StatelessWidget {
+  const AdminLogin({Key? key}) : super(key: key);
+
+  static String routeName = "/admin-login";
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Body(),
+    );
+  }
+}
