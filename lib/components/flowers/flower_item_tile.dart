38ebac9a9a5b216cb4fe79cef41d6903379b77c4
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-03-14 10:43:32 +0530

    Changed flower item tile to accept a model of flower with id.

--- lib/components/flowers/flower_item_tile.dart
+++ lib/components/flowers/flower_item_tile.dart
@@ -1,10 +1,11 @@
 import 'package:flower_info/models/flower_model.dart';
+import 'package:flower_info/models/flower_model_with_id.dart';
 import 'package:flutter/material.dart';
 
 class FlowerItemTile extends StatelessWidget {
-  const FlowerItemTile({Key? key, required this.flower}) : super(key: key);
+  const FlowerItemTile({Key? key, required this.flowerWithId}) : super(key: key);
 
-  final Flower flower;
+  final FlowerWithId flowerWithId;
 
   @override
   Widget build(BuildContext context) {
@@ -21,14 +22,14 @@ class FlowerItemTile extends StatelessWidget {
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(10.0),
                   child: Image.network(
-                    flower.imageLink,
+                    flowerWithId.imageLink,
                     height: 100,
                     width: 100,
                     fit: BoxFit.cover,
                   ),
                 ),
               ),
-              Text(flower.commonName)
+              Text(flowerWithId.commonName)
             ],
           ),
         ),
