290d074e69f80087634e6c571d3ec1db5ca65ef3
Navinda Lankesh <adilanga@gmail.com>
2022-03-14 20:15:46 +0530

    Disease - Admin Single View

--- lib/components/diseases/disease_item_tile_admin.dart
+++ lib/components/diseases/disease_item_tile_admin.dart
@@ -1,6 +1,7 @@
 import 'package:flower_info/api/disease_api.dart';
 import 'package:flower_info/models/disease_model_id.dart';
 import 'package:flower_info/screens/diseases/disease_edit.dart';
+import 'package:flower_info/screens/diseases/disease_view.dart';
 import 'package:flutter/material.dart';
 
 class DiseaseItemTileAdmin extends StatelessWidget {
@@ -20,95 +21,104 @@ class DiseaseItemTileAdmin extends StatelessWidget {
       );
     }
 
-    return Card(
-      elevation: 8,
-      margin: const EdgeInsets.all(10),
-      child: Container(
-        height: 100,
-        child: Row(
-          children: [
-            Center(
-              child: Image.network(disease.image),
-            ),
-            Expanded(
-              child: Container(
-                alignment: Alignment.topLeft,
-                child: Column(
-                  children: [
-                    Expanded(
-                      flex: 5,
-                      child: ListTile(
-                        title: Text(
-                          disease.name,
-                          style: const TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.w500,
+    return GestureDetector(
+      child: Card(
+        elevation: 8,
+        margin: const EdgeInsets.all(10),
+        child: Container(
+          height: 100,
+          child: Row(
+            children: [
+              Center(
+                child: Image.network(disease.image),
+              ),
+              Expanded(
+                child: Container(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 5,
+                        child: ListTile(
+                          title: Text(
+                            disease.name,
+                            style: const TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 5,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              Navigator.pushNamed(
-                                context,
-                                DiseaseEdit.routeName,
-                                arguments: DiseaseSingleView(disease),
-                              );
-                            },
-                            icon: const Icon(Icons.edit),
-                          ),
-                          const SizedBox(
-                            width: 8,
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.delete_forever),
-                            onPressed: () {
-                              showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    title: const Text('Delete This Disease?'),
-                                    content: const Text(
-                                        'This will delete the Disease permanently!'),
-                                    actions: [
-                                      ElevatedButton(
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                          child: const Text('Cancel')),
-                                      ElevatedButton(
-                                          onPressed: () {
-                                            Future<void> result =
-                                                _deleteDisease(disease);
-                                            Navigator.of(context).pop();
-                                            _notification("Deleted");
-                                          },
-                                          child: const Text('Yes'))
-                                    ],
-                                  );
-                                },
-                              );
-                            },
-                          ),
-                          const SizedBox(
-                            width: 8,
-                          )
-                        ],
-                      ),
-                    )
-                  ],
+                      Expanded(
+                        flex: 5,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                Navigator.pushNamed(
+                                  context,
+                                  DiseaseEdit.routeName,
+                                  arguments: DiseaseSingleView(disease),
+                                );
+                              },
+                              icon: const Icon(Icons.edit),
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.delete_forever),
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: const Text('Delete This Disease?'),
+                                      content: const Text(
+                                          'This will delete the Disease permanently!'),
+                                      actions: [
+                                        ElevatedButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text('Cancel')),
+                                        ElevatedButton(
+                                            onPressed: () {
+                                              Future<void> result =
+                                                  _deleteDisease(disease);
+                                              Navigator.of(context).pop();
+                                              _notification("Deleted");
+                                            },
+                                            child: const Text('Yes'))
+                                      ],
+                                    );
+                                  },
+                                );
+                              },
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
                 ),
+                flex: 8,
               ),
-              flex: 8,
-            ),
-          ],
+            ],
+          ),
         ),
       ),
+      onTap: () {
+        Navigator.pushNamed(
+          context,
+          DiseaseView.routeName,
+          arguments: DiseaseSingleView(disease),
+        );
+      },
     );
   }
 
