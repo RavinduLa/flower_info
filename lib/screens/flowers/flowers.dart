b15995808818fb5b9bae73ba832ff11e7acf1cd1
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-03-14 10:56:49 +0530

    Edited flowers.dart to retrieve data from the db.

--- lib/screens/flowers/flowers.dart
+++ lib/screens/flowers/flowers.dart
@@ -1,8 +1,10 @@
+import 'package:flower_info/api/firebase_api.dart';
 import 'package:flower_info/components/flowers/flower_item_tile.dart';
 import 'package:flower_info/data/flower_data.dart';
 import 'package:flutter/material.dart';
 
 import '../../models/flower_model.dart';
+import '../../models/flower_model_with_id.dart';
 
 class FLowers extends StatelessWidget {
   const FLowers({Key? key}) : super(key: key);
@@ -12,14 +14,30 @@ class FLowers extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.all(8),
-      child: GridView.builder(
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemCount: flowers.length,
-        itemBuilder: (context, index) {
-          return FlowerItemTile(flower: flowers[index]);
+      padding: const EdgeInsets.all(8.0),
+      child: StreamBuilder(
+        stream: FirebaseApi.flowersWithId,
+        builder: (BuildContext context,AsyncSnapshot<List<FlowerWithId>> snapshot){
+          if(snapshot.hasError){
+            return const Center(child: Text('Oops.. Something went wrong', style: TextStyle(fontSize: 30),),);
+          }
+          if(snapshot.hasData){
+            return GridView.builder(
+              gridDelegate:
+              const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemCount: snapshot.data!.length,
+              itemBuilder: (BuildContext context,int index) {
+                return FlowerItemTile(flowerWithId : snapshot.data![index]);
+              },
+            );
+          }
+          else{
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
         },
+
       ),
     );
   }
