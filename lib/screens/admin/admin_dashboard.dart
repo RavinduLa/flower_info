37267ac37c561cad1c2d74802fa45ca6d25fb237
Ravindu Wataketiya <57728932+RavinduLa@users.noreply.github.com>
2022-03-17 15:40:45 +0530

    Added cards in admin dashboard.

--- lib/screens/admin/admin_dashboard.dart
+++ lib/screens/admin/admin_dashboard.dart
@@ -1,4 +1,3 @@
-
 import 'package:flower_info/screens/diseases/disease_admin.dart';
 import 'package:flower_info/screens/flowers/flower_tests.dart';
 import 'package:flutter/material.dart';
@@ -16,36 +15,81 @@ class AdminDashboard extends StatelessWidget {
       appBar: AppBar(
         title: Text('Admin Dashboard'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, FlowerAdminList.routeName);
-            },
-            child: Text(
-              'Flower Panel',
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                child: const Card(
+                  color: Colors.purple,
+                  child: SizedBox(
+                    height: 100,
+                    child: Center(
+                      child: Text("Flower Panel", style: TextStyle(fontSize: 30),),
+                    ),
+                  ),
+                ),
+                onTap: () => Navigator.pushNamed(context, FlowerAdminList.routeName),
+              ),
             ),
-
-          ),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text('Fertilizers Panel'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, DiseaseAdmin.routeName);
-            },
-            child: Text('Diesases Panel'),
-          ),
-
-          ElevatedButton(
-            onPressed: () {
-              Navigator.pushNamed(context, FlowerTest.routeName);
-            },
-            child: Text('Flower Tests'),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                child: const Card(
+                  color: Colors.green,
+                  child: SizedBox(
+                    height: 100,
+                    child: Center(
+                      child: Text("Fertilizers Panel", style: TextStyle(fontSize: 30),),
+                    ),
+                  ),
+                ),
+                //onTap: () => Navigator.pushNamed(context, FlowerAdminList.routeName),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                child: const Card(
+                  color: Colors.amber,
+                  child: SizedBox(
+                    height: 100,
+                    child: Center(
+                      child: Text("Diseases Panel", style: TextStyle(fontSize: 30),),
+                    ),
+                  ),
+                ),
+                onTap: () => Navigator.pushNamed(context,DiseaseAdmin.routeName),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, FlowerAdminList.routeName);
+              },
+              child: Text(
+                'Flower Panel',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Fertilizers Panel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, DiseaseAdmin.routeName);
+              },
+              child: Text('Diesases Panel'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, FlowerTest.routeName);
+              },
+              child: Text('Flower Tests'),
+            ),
+          ],
+        ),
       ),
     );
   }
