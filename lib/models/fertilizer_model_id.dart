94a65ecc9aea76684af3e9e45416b77e6bc4ce79
Hirush Gimhan <it19240848@my.sliit.lk>
2022-03-19 00:25:19 +0530

    Created new class for Updating purpose

---
+++ lib/models/fertilizer_model_id.dart
@@ -0,0 +1,54 @@
+class FertilizerWithId {
+  final String documentId;
+  final String brandName;
+  final String type;
+  final String nitrogienValue;
+  final String phosporosValue;
+  final String potasiamValue;
+  final String description;
+  final String image;
+
+  const FertilizerWithId({
+    required this.documentId,
+    required this.brandName,
+    required this.type,
+    required this.nitrogienValue,
+    required this.phosporosValue,
+    required this.potasiamValue,
+    required this.description,
+    required this.image,
+  });
+
+  Map<String, dynamic> toJson() => {
+    'documentId': documentId,
+    'brandName': brandName,
+    'type': type,
+    'nitrogienValue': nitrogienValue,
+    'phosporosValue': phosporosValue,
+    'potasiamValue': potasiamValue,
+    'description': description,
+    'image': image,
+  };
+
+  static FertilizerWithId fromJson(Map<String, dynamic> json) => FertilizerWithId(
+    documentId: json['documentId'],
+    brandName: json['brandName'],
+    type: json['type'],
+    nitrogienValue: json['nitrogienValue'],
+    phosporosValue: json['phosporosValue'],
+    potasiamValue: json['potasiamValue'],
+    description: json['description'],
+    image: json['image'],
+  );
+
+  Map<String, dynamic> toJsonForUpdate() => {
+    'documentId': documentId,
+    'brandName': brandName,
+    'type': type,
+    'nitrogienValue': nitrogienValue,
+    'phosporosValue': phosporosValue,
+    'potasiamValue': potasiamValue,
+    'description': description,
+    'image': image,
+  };
+}
