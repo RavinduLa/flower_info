e740f6392314b91c3f2d91b9e6a6b80f7a07edc0
Hirush Gimhan <it19240848@my.sliit.lk>
2022-03-24 01:48:40 +0530

    Added onTap Inkwells

--- lib/components/fertilizers/fertilizer_item_tile_admin.dart
+++ lib/components/fertilizers/fertilizer_item_tile_admin.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 
 class FertilizerItemTileAdmin extends StatelessWidget {
   final FertilizerWithId fertilizer;
+
   const FertilizerItemTileAdmin({Key? key, required this.fertilizer})
       : super(key: key);
 
@@ -35,148 +36,152 @@ class FertilizerItemTileAdmin extends StatelessWidget {
         )),
         elevation: 4,
         margin: const EdgeInsets.only(left: 8, right: 8, top: 8),
-        child: SizedBox(
-          height: 100,
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 10),
-                child: SizedBox(
-                  width: 80,
-                  height: 80,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10.0),
-                    child: CachedNetworkImage(
-                      imageUrl: fertilizer.image,
-                      width: 80,
-                      height: 80,
-                      fit: BoxFit.cover,
-                      errorWidget: (context, url, error) =>
-                          const Icon(Icons.error, size: 25),
-                      placeholder: (context, url) =>
-                          const CircularProgressIndicator(
-                        valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+        child: InkWell(
+          onTap: () {
+            Navigator.pushNamed(
+              context,
+              FertilizerView.routeName,
+              arguments: FertilizerSingleView(fertilizer),
+            );
+          },
+          child: SizedBox(
+            height: 100,
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: SizedBox(
+                    width: 80,
+                    height: 80,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: CachedNetworkImage(
+                        imageUrl: fertilizer.image,
+                        width: 80,
+                        height: 80,
+                        fit: BoxFit.cover,
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error, size: 25),
+                        placeholder: (context, url) =>
+                            const CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.green),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  alignment: Alignment.topLeft,
-                  child: Column(
-                    children: [
-                      Expanded(
-                        flex: 3,
-                        child: ListTile(
-                          title: Text(
-                            brandName,
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w500,
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.topLeft,
+                    child: Column(
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: ListTile(
+                            title: Text(
+                              brandName,
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w500,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: ListTile(
-                          title: Text(
-                            type,
-                            style: const TextStyle(
-                              fontSize: 15,
-                              fontWeight: FontWeight.w500,
+                        Expanded(
+                          flex: 2,
+                          child: ListTile(
+                            title: Text(
+                              type,
+                              style: const TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Expanded(
-                        flex: 5,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            IconButton(
-                              onPressed: () {
-                                Navigator.pushNamed(
-                                  context,
-                                  FertilizerEdit.routeName,
-                                  arguments: FertilizerSingleView(fertilizer),
-                                );
-                              },
-                              icon: const Icon(Icons.edit,
-                                  size: 30, color: Colors.green),
-                            ),
-                            const SizedBox(
-                              width: 8,
-                            ),
-                            IconButton(
-                              icon: const Icon(Icons.delete,
-                                  size: 30, color: Colors.red),
-                              onPressed: () {
-                                showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      title: const Text('Delete This Fertilizer?'),
-                                      content: Text(
-                                          'This will delete the $brandName permanently!'),
-                                      actions: [
-                                        ElevatedButton(
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                          child: const Text(
-                                            'Cancel',
+                        Expanded(
+                          flex: 5,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  Navigator.pushNamed(
+                                    context,
+                                    FertilizerEdit.routeName,
+                                    arguments: FertilizerSingleView(fertilizer),
+                                  );
+                                },
+                                icon: const Icon(Icons.edit,
+                                    size: 30, color: Colors.green),
+                              ),
+                              const SizedBox(
+                                width: 8,
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.delete,
+                                    size: 30, color: Colors.red),
+                                onPressed: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: const Text(
+                                            'Delete This Fertilizer?'),
+                                        content: Text(
+                                            'This will delete the $brandName permanently!'),
+                                        actions: [
+                                          ElevatedButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: const Text(
+                                              'Cancel',
+                                            ),
+                                            style: ButtonStyle(
+                                              backgroundColor:
+                                                  MaterialStateProperty.all(
+                                                      Colors.amber),
+                                            ),
                                           ),
-                                          style: ButtonStyle(
-                                            backgroundColor:
-                                                MaterialStateProperty.all(
-                                                    Colors.amber),
+                                          ElevatedButton(
+                                            onPressed: () {
+                                              Future<void> result =
+                                                  _deleteFertilizer(fertilizer);
+                                              Navigator.of(context).pop();
+                                              _notification(
+                                                  "Fertilizer Deleted!");
+                                            },
+                                            child: const Text('Yes'),
+                                            style: ButtonStyle(
+                                              backgroundColor:
+                                                  MaterialStateProperty.all(
+                                                      Colors.redAccent),
+                                            ),
                                           ),
-                                        ),
-                                        ElevatedButton(
-                                          onPressed: () {
-                                            Future<void> result =
-                                                _deleteFertilizer(fertilizer);
-                                            Navigator.of(context).pop();
-                                            _notification(
-                                                "Fertilizer Deleted!");
-                                          },
-                                          child: const Text('Yes'),
-                                          style: ButtonStyle(
-                                            backgroundColor:
-                                                MaterialStateProperty.all(
-                                                    Colors.redAccent),
-                                          ),
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              },
-                            ),
-                            const SizedBox(
-                              width: 8,
-                            )
-                          ],
-                        ),
-                      )
-                    ],
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                              ),
+                              const SizedBox(
+                                width: 8,
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
+                  flex: 8,
                 ),
-                flex: 8,
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
-      onTap: () {
-        Navigator.pushNamed(
-          context,
-          FertilizerView.routeName,
-          arguments: FertilizerSingleView(fertilizer),
-        );
-      },
     );
   }
 
